dcd2b144424c7a7424cf2e7055d87dee62965b58
mkaiho <mkaiho.cgengo@gmail.com>
2023-07-16 16:46:20 +0000

    add mui

--- src/app/Home.stories.tsx
+++ src/app/Home.stories.tsx
@@ -7,8 +7,6 @@ const meta: Meta<typeof Home> = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: "fullscreen",
     nextjs: {
       appDirectory: true,
     },

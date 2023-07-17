e7b25fb10ff9cf77ad10c9a5fb4c447fb34d0b78
mkaiho <mkaiho.cgengo@gmail.com>
2023-07-17 08:48:51 +0000

    add eslint rule to sort import order

--- src/app/Home.stories.tsx
+++ src/app/Home.stories.tsx
@@ -1,5 +1,6 @@
-import type { Meta, StoryObj } from "@storybook/react";
 import Home from "./page";
+
+import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof Home> = {
   title: "Example/Home",
